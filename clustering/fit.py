ce6c99ad4354872505c33d4edf2501ad65cde8e8
daicho <daichimax78@gmail.com>
2021-02-09 02:03:50 +0900

    "二次近似による判別を実装

---
+++ clustering/fit.py
@@ -0,0 +1,33 @@
+import os
+import glob
+import cv2
+import numpy as np
+
+import sklearn.metrics as metrics
+
+from const import *
+from image import *
+
+THIS_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# 二次近似により係数を算出
+def fit_image(img):
+    x = line_image(img)
+
+    for i in range(x.size):
+        p = np.polyfit(np.arange(x.size - i), x[i:], 2)
+        r2 = np.corrcoef(x[i:], np.poly1d(p)(np.arange(x.size - i)))[0][1]
+
+        if r2 >= 0.995:
+            print(p)
+            return p[0]
+
+    print("Error")
+
+if __name__ == "__main__":
+    img = cv2.imread(THIS_PATH + "/images/normal/017.jpg")
+    fit_image(img)
+
+    # process_images(THIS_PATH + "/images/normal", fit_image)
+    # print()
+    # process_images(THIS_PATH + "/images/0.6", fit_image)
--- clustering/image.py
+++ clustering/image.py
@@ -11,22 +11,30 @@ THIS_PATH = os.path.dirname(os.path.abspath(__file__))
 def trim_image(img, pos, size):
     return img[pos[1] : pos[1] + size[1], pos[0] : pos[0] + size[0]]
 
-# 画像加工
-def process_image(img):
-    img_temp = img.copy()
-    img_temp = trim_image(img_temp, TRIM_POS, TRIM_SIZE)
-    img_temp = cv2.medianBlur(img_temp, 99)
-    img_temp = cv2.cvtColor(img_temp, cv2.COLOR_BGR2GRAY)
-    img_temp = cv2.resize(img_temp, RES_SIZE)
-    return img_temp
+# 一次元化
+def line_image(img):
+    img_temp = trim_image(img, TRIM_POS, TRIM_SIZE)
+    x = sum(img_temp[:, :, i] for i in range(3)) / 765
+    x = np.sort(x, axis=0)
+    x = np.mean(x[TRIM_SIZE[1] // 2 - 100 : TRIM_SIZE[1] // 2 + 100], axis=0)
+    return x
 
 # 画像加工して保存
-def convert_images(path_in, path_out, convert_func, gray=False):
+def convert_images(path_in, path_out, convert_func):
     for path in glob.glob(path_in + "/*"):
-        img_org = cv2.imread(path, cv2.IMREAD_GRAYSCALE if gray else cv2.IMREAD_COLOR)
+        img_org = cv2.imread(path)
         img_dst = convert_func(img_org)
         cv2.imwrite(path_out + "/" + os.path.splitext(os.path.basename(path))[0] + ".png", img_dst)
 
+# フォルダ内の画像を処理
+def process_images(path_in, process_func):
+    for path in glob.glob(path_in + "/*"):
+        img_org = cv2.imread(path)
+        print(process_func(img_org))
+
 if __name__ == "__main__":
-    convert_images(THIS_PATH + "/images/normal", THIS_PATH + "/dataset/normal", process_image)
-    convert_images(THIS_PATH + "/images/0.6", THIS_PATH + "/dataset/0.6", process_image)
+    img = cv2.imread(THIS_PATH + "/images/normal/017.jpg")
+    x = line_image(img)
+
+    for t in x:
+        print(t)
